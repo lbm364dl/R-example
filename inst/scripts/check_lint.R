ba61769805f5d35b1663d44c922b986e2ef7f299
lbm364dl <geometrydashm364@gmail.com>
2025-01-08 17:45:01 +0100

    Fail lint

--- inst/scripts/check_lint.R
+++ inst/scripts/check_lint.R
@@ -1,3 +1,1 @@
-lintr::lint_package(
-  linters = lintr::linters_with_defaults(object_usage_linter = NULL)
-)
+lintr::lint_package(linters = lintr::linters_with_defaults(object_usage_linter = NULL))
