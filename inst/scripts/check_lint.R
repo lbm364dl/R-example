0f9c90a4c5141f1ad6645d2ba07811014266d4bf
lbm364dl <geometrydashm364@gmail.com>
2025-01-08 17:51:53 +0100

    Fix back lint

--- inst/scripts/check_lint.R
+++ inst/scripts/check_lint.R
@@ -1,1 +1,3 @@
-lintr::lint_package(linters = lintr::linters_with_defaults(object_usage_linter = NULL))
+lintr::lint_package(
+  linters = lintr::linters_with_defaults(object_usage_linter = NULL)
+)
