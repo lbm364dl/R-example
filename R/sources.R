7cba62e6b937246c2579178696f959694522e5dc
lbm364dl <geometrydashm364@gmail.com>
2025-01-09 14:20:08 +0100

    Restructure sources script

--- R/sources.R
+++ R/sources.R
@@ -1,55 +1,64 @@
+#' Create a new dataframe where each row has a year range into one where each
+#' row is a single year, effectively 'expanding' the whole year range
+#'
+#' @param trade_sources A tibble dataframe
+#' where each row contains the year range
+#'
+#' @return A tibble dataframe where each row
+#' corresponds to a single year for a given source
+#'
+#' @export
+#'
+#' @examples
+#' trade_sources <- tibble::tibble(
+#'   Name = c("a", "b", "c"),
+#'   Trade = c("t1", "t2", "t3"),
+#'   Info_Format = c("year", "partial_series", "year"),
+#'   Timeline_Start = c(1, 1, 2),
+#'   Timeline_End = c(3, 4, 5),
+#'   Timeline_Freq = c(1, 1, 2),
+#'   `Imp/Exp` = "Imp",
+#'   SACO_link = NA,
+#' )
+#' expand_trade_sources(trade_sources)
+expand_trade_sources <- function(trade_sources) {
+  trade_sources |>
+    dplyr::filter(is.na(Trade) == 0) |>
+    .expand_trade_years() |>
+    dplyr::mutate(
+      Name = dplyr::if_else(
+        Info_Format == "year", paste(Name, Year, sep = "_"), Name
+      ),
+      ImpExp = `Imp/Exp`,
+      In_Saco = as.integer(!is.na(SACO_link)),
+    )
+}
+
+.expand_trade_years <- function(trade_sources) {
+  trade_sources <- dplyr::mutate(trade_sources, No = dplyr::row_number())
+
+  trade_sources |>
+    dplyr::group_by(No) |>
+    tidyr::expand(Year = seq(Timeline_Start, Timeline_End, Timeline_Freq)) |>
+    dplyr::inner_join(trade_sources, by = "No")
+}
+
+
 # Step 1: Authentication
 googlesheets4::gs4_auth()
+sheet_url <- "1UdwgS87x5OsLjNuKaY3JA01GoI5nwsenz62JXCeq0GQ"
 
-# PART 1: sources_trade FOR TRADE
+# PART 1: trade_sources FOR TRADE
 
 # Step 2: Rest of Program
-# Read a Google Sheet after authentication
-sheet_url <- "1UdwgS87x5OsLjNuKaY3JA01GoI5nwsenz62JXCeq0GQ"
-sources_trade <- googlesheets4::read_sheet(
-  sheet_url,
-  sheet = "Final_Sources_Trade"
-)
-
-sources_trade <- dplyr::filter(sources_trade, is.na(Trade) == 0)
-
-# Expand data into individual years
-expanded_sources_trade <- do.call(rbind, lapply(1:nrow(sources_trade), function(i) {
-  data.frame(
-    Number = sources_trade$No[i],
-    Added_By = sources_trade$Added_By[i],
-    APA_Reference = sources_trade$APA_Reference[i],
-    Name = sources_trade$Name[i],
-    Reporter = sources_trade$Reporter[i],
-    Trade = sources_trade$Trade[i],
-    ImpExp = sources_trade$`Imp/Exp`[i],
-    Measurement = sources_trade$Measurement[i],
-    Doc_Format = sources_trade$Doc_Format[i],
-    Quality = sources_trade$Quality[i],
-    Info_Format = sources_trade$Info_Format[i],
-    Scanned = sources_trade$Scanned[i],
-    External_Link = sources_trade$External_link[i],
-    Saco_Link = sources_trade$SACO_link[i],
-    In_Saco = ifelse(is.na(sources_trade$SACO_link[i]) == 0, 1, 0),
-    Notes = sources_trade$Notes[i],
-    Year = seq(
-      sources_trade$Timeline_Start[i],
-      sources_trade$Timeline_End[i],
-      sources_trade$Timeline_Freq[i]
-    )
-  )
-}))
-
-expanded_sources_trade <- expanded_sources_trade |>
-  dplyr::mutate(
-    Name = dplyr::if_else(
-      Info_Format == "year", paste(Name, Year, sep = "_"), Name
-    )
-  )
+expanded_trade_sources <-
+  sheet_url |>
+  googlesheets4::read_sheet(sheet = "Final_Sources_Trade") |>
+  expand_trade_sources()
 
 # Plot showing years covered by sources_trade
 plot_sources_trade <- ggplot2::ggplot(
-  expanded_sources_trade,
+  expanded_trade_sources,
   ggplot2::aes(y = Trade, x = Year, fill = "lightblue")
 ) +
   ggplot2::geom_tile(alpha = .8) +
@@ -61,7 +70,7 @@ plot_sources_trade
 
 # Plot showing by years, colored by sources_trade
 plot_sources_trade_sources <- ggplot2::ggplot(
-  expanded_sources_trade,
+  expanded_trade_sources,
   ggplot2::aes(y = Trade, x = Year, fill = Name, alpha = .8)
 ) +
   ggplot2::geom_tile() +
@@ -73,7 +82,7 @@ plot_sources_trade_sources
 
 # Plot showing years covered by sources_trade, colored by quality
 plot_sources_trade_quality <- ggplot2::ggplot(
-  expanded_sources_trade,
+  expanded_trade_sources,
   ggplot2::aes(y = Trade, x = Year, alpha = .8)
 ) +
   ggplot2::geom_tile(ggplot2::aes(fill = factor(Quality))) +
@@ -88,7 +97,7 @@ plot_sources_trade_quality
 
 # Plot showing years covered by sources_trade, colored by scanned/not scanned
 plot_sources_trade_scanned <- ggplot2::ggplot(
-  expanded_sources_trade,
+  expanded_trade_sources,
   ggplot2::aes(y = Trade, x = Year)
 ) +
   ggplot2::geom_tile(ggplot2::aes(fill = factor(Scanned), alpha = .8)) +
@@ -103,7 +112,7 @@ plot_sources_trade_scanned
 
 # Plot showing years covered by sources_trade, colored by whether or not in SACO
 plot_sources_trade_saco <- ggplot2::ggplot(
-  expanded_sources_trade,
+  expanded_trade_sources,
   ggplot2::aes(y = Trade, x = Year, alpha = .8)
 ) +
   ggplot2::geom_tile(ggplot2::aes(fill = factor(In_Saco))) +
@@ -115,13 +124,3 @@ plot_sources_trade_saco <- ggplot2::ggplot(
   ) +
   ggplot2::facet_grid(ImpExp ~ Reporter, scales = "free", space = "free_x")
 plot_sources_trade_saco
-
-
-# PART 2: sources_trade FOR PRODUCTION
-
-# Step 3: Rest of Program
-sources_prod <- googlesheets4::read_sheet(
-  sheet_url,
-  sheet = "Final_Sources_Production"
-)
-sources_prod <- dplyr::filter(sources_prod, is.na(Name) == 0)
