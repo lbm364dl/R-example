3108d827baceafb3866b52ed876f3fde0018e16c
lbm364dl <geometrydashm364@gmail.com>
2025-01-21 10:05:37 +0100

    Update helper scripts

--- inst/scripts/run_check_with_renv.R
+++ inst/scripts/run_check_with_renv.R
@@ -1,8 +1,1 @@
-# Temporary R_LIBS setup for devtools::check() with renv
-Sys.setenv(R_LIBS = renv::paths$library())
-
-# Run the check
 devtools::check()
-
-# Restore the original R_LIBS
-Sys.unsetenv("R_LIBS")
